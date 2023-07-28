55fea6821919daf8440c271b183c2328ee2aedbc
Yuanjing Hong <v1nh1shungry@outlook.com>
2023-07-28 13:56:19 +0800

    feat: add `cppman` support

--- README.md
+++ README.md
@@ -1,39 +1,80 @@
 # cppreference.nvim
 
-A deadly silly plugin for opening [cppreference](http://cppreference.com/) page in your browser from your favorite editor.
+A deadly silly plugin for searching and viewing [cppreference](http://cppreference.com/) pages in your favorite editor.
+
+**NOTE**: currently only Linux is supported!
 
 # Showcase
 
+* Browser:
+
 ![](https://user-images.githubusercontent.com/98312435/256507363-f7e826b7-a340-4e42-ade6-669d569853c0.gif)
+
+* [cppman](https://github.com/aitjcize/cppman):
+
+![](https://user-images.githubusercontent.com/98312435/256728366-b409f9a0-2583-4e0e-b583-7c86a24c353f.gif)
 
 **NOTE**: the GIF is kind of broken because of the screen recorder. I may fix it when available.
 
+# Features
+
+* Dual view: your favorite browser, or [cppman](https://github.com/aitjcize/cppman)! You don't even have to leave dear neovim.
+
+* Fuzzy search (powered by [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)): what you can expect from a [vscode extension](https://github.com/Guyutongxue/VSC_CppReference) or a [browser extension](https://github.com/huhu/cpp-search-extension) is now available in the great neovim!
+
 # Installation
 
-1. Make sure you have `curl` installed available in `$PATH`.
+1. Make sure you have `curl` installed and be available in `$PATH`.
 
-2. [lazy.nvim](https://github.com/folke/lazy.nvim):
+2. * if you use the browser, make sure `xdg-open` is available in `$PATH` which is used to open the browser.
+   
+   * if you use `cppman`, make sure you have `cppman` installed and be available in `$PATH`. And make sure you have set the `cppman`'s source to `cppreference.com` (which is the default value. If you haven't modified this option after the installation you don't have to do anything).
+
+3. [lazy.nvim](https://github.com/folke/lazy.nvim):
    
    ```lua
    {
-    'v1nh1shungry/cppreference.nvim'
-    config = true,
-    dependencies = 'nvim-telescope/telescope.nvim',
+     'v1nh1shungry/cppreference.nvim'
+     dependencies = 'nvim-telescope/telescope.nvim',
+     opts = {},
    }
    ```
 
+# Configuration
+
+```lua
+-- default configuration
+require('cppreference').setup {
+  -- viewer to display the cppreference page
+  -- can be 'browser' or 'cppman'
+  view = 'browser',
+}
+```
+
 # Usage
 
-* `require('cppreference').fuzzy_search()`
+* `require('cppreference').fuzzy_search(view)`:
+  Use `view` as the viewer despite the configuration. Again, `view` can be either `browser` or `cppman`. If the parameter is absent the configuration will be used.
 
 * `require('cppreference').update_index()`
+
+# FAQ ~~(actually nobody asks)~~
+
+* **Q:** An entry exists in the fuzzy search pane, but `cppman` says it can't locate it?
+  **A:** The plugin eventually relies on `cppman` to locate the page if you use `cppman` as the viewer, and `cppman` can only locate objects own an individual page. In this case you have to use the browser to open the URL, which can be found in the index file.
 
 # TODO
 
 - [ ] `require('cppreference').open(word)`: directly offer the keyword, useful to play with `expand('<cword>')`
 
-- [ ] Seriously it's much better to open [cppman](https://github.com/aitjcize/cppman) instead of browser, but it's kind of hard to hack in.
+- [ ] Windows support
+
+- [x] Seriously it's much better to open [cppman](https://github.com/aitjcize/cppman) instead of browser, but it's kind of hard to hack in.
+  
+  - [ ] `keywordprg` and the `K` key support
 
 # Credits
 
 * [Guyutongxue/VSC_CppReference](https://github.com/Guyutongxue/VSC_CppReference): the plugin uses the index from [Guyutongxue/cppreference-index](https://github.com/Guyutongxue/cppreference-index).
+
+* [skywind3000/vim-cppman](https://github.com/skywind3000/vim-cppman)
--- lua/cppreference.lua
+++ lua/cppreference.lua
@@ -3,6 +3,9 @@ local M = {}
 local index_path = vim.fn.stdpath('data') .. '/cppreference.json'
 local job = nil
 local index = nil
+local options = {
+  view = 'browser',
+}
 
 local log = function(msg, level)
   vim.notify(msg, level, { title = 'cppreference.nvim' })
@@ -33,7 +36,8 @@ M.update_index = function(on_exit)
   end)
 end
 
-M.setup = function()
+M.setup = function(opts)
+  options = vim.tbl_extend('force', options, opts or {})
   local setup = function()
     index = vim.json.decode(vim.fn.join(vim.fn.readfile(index_path), '\n'))
   end
@@ -44,7 +48,83 @@ M.setup = function()
   end
 end
 
-M.fuzzy_search = function()
+local open_browser = function(url)
+  if vim.fn.executable('xdg-open') == 0 then
+    error('Requires `xdg-open` to open the page in the default browser')
+  else
+    vim.system { 'xdg-open', url }
+  end
+end
+
+local cppman = function(keyword)
+  if vim.fn.executable('cppman') == 0 then
+    error('`cppman` is not available in $PATH')
+    return
+  end
+
+  vim.system({
+    'cppman',
+    '--force-columns=' .. vim.fn.winwidth(0) - 2,
+    keyword
+  }, {}, vim.schedule_wrap(function(res)
+    if string.find(res.stdout, 'No manual entry') then
+      error("`cppman` can't locate this keyword, please use browser instead")
+      return
+    end
+    local content = vim.split(res.stdout, '\n')
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    local bufset = function(name, value) vim.api.nvim_set_option_value(name, value, { buf = buf }) end
+    bufset('buftype', 'nofile')
+    bufset('swapfile', false)
+    bufset('bufhidden', 'delete')
+    bufset('ft', 'man')
+    bufset('readonly', false)
+    bufset('modifiable', true)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, content)
+    vim.api.nvim_buf_set_name(buf, 'man://cppman/' .. keyword)
+    bufset('readonly', true)
+    bufset('modifiable', false)
+
+    vim.cmd.split()
+    local win = vim.api.nvim_get_current_win()
+    local winset = function(name, value)
+      vim.api.nvim_set_option_value(name, value, { win = win })
+    end
+    vim.api.nvim_win_set_buf(win, buf)
+    winset('number', false)
+    winset('relativenumber', false)
+    winset('signcolumn', 'no')
+    winset('colorcolumn', '0')
+    vim.cmd 'normal! gg'
+
+    -- set up highlight
+    -- https://github.com/skywind3000/vim-cppman/blob/master/plugin/cppman.vim
+    vim.cmd [[
+  syntax clear
+  syntax case ignore
+  syntax match manReference       "[a-z_:+-\*][a-z_:+-~!\*<>]\+([1-9][a-z]\=)"
+  syntax match manTitle           "^\w.\+([0-9]\+[a-z]\=).*"
+  syntax match manSectionHeading  "^[a-z][a-z_ \-:]*[a-z]$"
+  syntax match manSubHeading      "^\s\{3\}[a-z][a-z ]*[a-z]$"
+  syntax match manOptionDesc      "^\s*[+-][a-z0-9]\S*"
+  syntax match manLongOptionDesc  "^\s*--[a-z0-9-]\S*"
+  syntax include @cppCode runtime! syntax/cpp.vim
+  syntax match manCFuncDefinition  display "\<\h\w*\>\s*("me=e-1 contained
+  syntax region manSynopsis start="^SYNOPSIS"hs=s+8 end="^\u\+\s*$"me=e-12 keepend contains=manSectionHeading,@cppCode,manCFuncDefinition
+  syntax region manSynopsis start="^EXAMPLE"hs=s+7 end="^       [^ ]"he=s-1 keepend contains=manSectionHeading,@cppCode,manCFuncDefinition
+  hi def link manTitle    Title
+  hi def link manSectionHeading  Statement
+  hi def link manOptionDesc    Constant
+  hi def link manLongOptionDesc  Constant
+  hi def link manReference    PreProc
+  hi def link manSubHeading      Function
+  hi def link manCFuncDefinition Function
+  ]]
+  end))
+end
+
+M.fuzzy_search = function(view)
   if job then
     if not job:is_closing() then
       info('Fetching the latest index, please wait until it finishes')
@@ -76,13 +156,11 @@ M.fuzzy_search = function()
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        if vim.fn.executable('xdg-open') == 0 then
-          error('Requires `xdg-open` to open the page in the default browser')
+        view = view or options.view
+        if view == 'browser' then
+          open_browser('https://en.cppreference.com/w/' .. selection.value)
         else
-          vim.system {
-            'xdg-open',
-            'https://en' .. '.cppreference.com/w/' .. selection.value,
-          }
+          cppman(selection.display)
         end
       end)
       return true
